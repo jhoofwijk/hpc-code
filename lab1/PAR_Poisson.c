3b56cd94a068cf463460c6226e4c8bd85376a66f
Jorn Hoofwijk <jornhoofwijk@gmail.com>
2019-12-27 18:52:58 +0100

    step 9

--- lab1/PAR_Poisson.c
+++ lab1/PAR_Poisson.c
@@ -319,13 +319,14 @@ void Solve()
   int count = 0;
   double delta;
   double delta1, delta2;
+  double global_delta;
 
   Debug("Solve", 0);
 
   /* give global_delta a higher value then precision_goal */
-  delta = 2 * precision_goal;
+  global_delta = 2 * precision_goal;
 
-  while (delta > precision_goal && count < max_iter)
+  while (global_delta > precision_goal && count < max_iter)
   {
     Debug("Do_Step 0", 0);
     delta1 = Do_Step(0);
@@ -336,6 +337,8 @@ void Solve()
     Exchange_Borders();
 
     delta = max(delta1, delta2);
+    MPI_Allreduce(&delta, &global_delta, 1, MPI_DOUBLE, MPI_MAX, grid_comm);
+
     count++;
   }
 
