8bae92a3a8915c233b28c7276d52787c7cecd9ff
Jorn Hoofwijk <jornhoofwijk@gmail.com>
2020-01-02 15:46:55 +0100

    merge separate cuda files into a single one

--- lab3/power_gpu.cu
+++ lab3/power_gpu.cu
@@ -36,7 +36,7 @@ void Cleanup(void);
 void InitOne(float*, int);
 void UploadArray(float*, int);
 float CPUReduce(float*, int);
-void  Arguments(int, char**);
+void ParseArguments(int, char**);
 void checkCardVersion(void);
 
 // Kernels
@@ -279,4 +279,164 @@ void checkCardVersion()
       fprintf(stderr,"Need compute capability 2 or higher.\n");
       exit(1);
    }
+}
+
+
+
+/*****************************************************************************
+This function finds the product of Matrix A and vector V
+*****************************************************************************/
+
+// ****************************************************************************************************************************************************/
+// parallelization method for the Matrix-vector multiplication as follows: 
+
+// each thread handle a multiplication of each row of Matrix A and vector V;
+
+// The share memory is limited for a block, instead of reading an entire row of matrix A or vector V from global memory to share memory, 
+// a square submatrix of A is shared by a block, the size of square submatrix is BLOCK_SIZE*BLOCK_SIZE; Thus, a for-loop is used to
+// handle a multiplication of each row of Matrix A and vector V step by step. In eacg step, two subvectors with size BLOCK_SIZE is multiplied.
+//*****************************************************************************************************************************************************/
+
+
+__global__ void Av_Product(float* g_MatA, float* g_VecV, float* g_VecW, int N)
+{
+    // Block index
+    int bx = blockIdx.x;
+
+    // Thread index
+    int tx = threadIdx.x;
+
+    int aBegin = N * BLOCK_SIZE * bx;
+
+    int aEnd   = aBegin + N - 1;
+    int step  = BLOCK_SIZE;
+
+    int bBegin = 0;//BLOCK_SIZE * bx;
+    int bIndex=0;
+    int aIndex =0;
+    float Csub = 0;
+
+    for (int a = aBegin, b = bBegin;
+         a <= aEnd;
+         a += step, b += step)
+    {
+
+        __shared__ float As[BLOCK_SIZE*BLOCK_SIZE];
+
+        __shared__ float bs[BLOCK_SIZE];
+        
+
+        for (int aa = 0; aa < BLOCK_SIZE;aa+= 1)
+        {
+            aIndex = a+tx+aa*N;
+            if( aIndex < N*N)
+        	    As[tx+aa*BLOCK_SIZE] = g_MatA[aIndex];
+		        else
+        	    As[tx+aa*BLOCK_SIZE] = 0;
+        }
+
+        bIndex = b+tx;
+   	    if(bIndex<N)   
+		      bs[tx] = g_VecV[bIndex];
+	      else
+		      bs[tx] = 0;
+
+        __syncthreads();
+
+        for (int k = 0; k < BLOCK_SIZE; ++k)
+        {
+            Csub += As[k+tx*BLOCK_SIZE] * bs[k];
+        }//}
+        __syncthreads();
+    }
+
+    g_VecW[ BLOCK_SIZE * bx + tx] = Csub;
+}
+
+
+
+
+
+__global__ void ComputeLamda( float* g_VecV, float* g_VecW, float * g_Lamda,int N)
+{
+  // shared memory size declared at kernel launch
+  extern __shared__ float sdataVW[];
+  unsigned int tid = threadIdx.x;
+  unsigned int globalid = blockIdx.x*blockDim.x + threadIdx.x;
+
+  // For thread ids greater than data space
+  if (globalid < N) {
+     sdataVW[tid] =  g_VecV[globalid] * g_VecW[globalid];
+  }
+  else {
+     sdataVW[tid] = 0;  // Case of extra threads above N
+  }
+
+  // each thread loads one element from global to shared mem
+  __syncthreads();
+
+  // do reduction in shared mem
+  for (unsigned int s=blockDim.x / 2; s > 0; s = s >> 1) {
+     if (tid < s) {
+         sdataVW[tid] = sdataVW[tid] + sdataVW[tid+ s];
+     }
+     __syncthreads();
+  }
+   // atomic operations:
+  if (tid == 0) atomicAdd(g_Lamda,sdataVW[0]);
+}
+
+
+
+/****************************************************
+Normalizes vector W : W/norm(W)
+****************************************************/
+__global__ void FindNormW(float* g_VecW, float * g_NormW, int N)
+{
+  // shared memory size declared at kernel launch
+  extern __shared__ float sdata[];
+  unsigned int tid = threadIdx.x;
+  unsigned int globalid = blockIdx.x*blockDim.x + threadIdx.x;
+
+  // For thread ids greater than data space
+  if (globalid < N) {
+     sdata[tid] =  g_VecW[globalid];
+  }
+  else {
+     sdata[tid] = 0;  // Case of extra threads above N
+  }
+
+  // each thread loads one element from global to shared mem
+  __syncthreads();
+
+  sdata[tid] = sdata[tid] * sdata[tid];
+  __syncthreads();
+
+  // do reduction in shared mem
+  for (unsigned int s=blockDim.x / 2; s > 0; s = s >> 1) {
+     if (tid < s) {
+         sdata[tid] = sdata[tid] + sdata[tid+ s];
+     }
+     __syncthreads();
+  }
+   // atomic operations:
+  if (tid == 0) atomicAdd(g_NormW,sdata[0]);
+}
+
+
+__global__ void NormalizeW(float* g_VecW, float * g_NormW, float* g_VecV, int N)
+{
+  // shared memory size declared at kernel launch
+  extern __shared__ float sNormData[];
+  unsigned int tid = threadIdx.x;
+  unsigned int globalid = blockIdx.x*blockDim.x + threadIdx.x;
+
+  if(tid==0) sNormData[0] =  g_NormW[0];
+  __syncthreads();
+
+  // For thread ids greater than data space
+  if (globalid < N) {
+     g_VecV[globalid] = g_VecW[globalid]/sNormData[0];
+  }
+
 }
