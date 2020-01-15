47b76b66351e179b07f206c1e80bf8e7a63edd55
Jorn Hoofwijk <jornhoofwijk@gmail.com>
2020-01-15 16:35:59 +0100

    normalizeW with global mem

--- lab3/power_gpu_global.cu
+++ lab3/power_gpu_global.cu
@@ -471,17 +471,11 @@ __global__ void FindNormW(float* g_VecW, float * g_NormW, int N)
 
 __global__ void NormalizeW(float* g_VecW, float * g_NormW, float* g_VecV, int N)
 {
-  // shared memory size declared at kernel launch
-  extern __shared__ float sNormData[];
-  unsigned int tid = threadIdx.x;
   unsigned int globalid = blockIdx.x*blockDim.x + threadIdx.x;
-
-  if(tid==0) sNormData[0] =  g_NormW[0];
   __syncthreads();
 
   // For thread ids greater than data space
   if (globalid < N) {
-     g_VecV[globalid] = g_VecW[globalid]/sNormData[0];
+     g_VecV[globalid] = g_VecW[globalid] / g_NormW[0];
   }
-
 }
