9af24e5372f6291459aeb35ccd2d62ed743ccf1c
Jorn Hoofwijk <jornhoofwijk@gmail.com>
2020-01-15 16:51:10 +0100

    compute lambda in global mem

--- lab3/power_gpu_global.cu
+++ lab3/power_gpu_global.cu
@@ -405,16 +405,13 @@ __global__ void Av_Product(float* g_MatA, float* g_VecV, float* g_VecW, int N)
 __global__ void ComputeLamda( float* g_VecV, float* g_VecW, float * g_Lamda,int N)
 {
   // shared memory size declared at kernel launch
-  extern __shared__ float sdataVW[];
   unsigned int tid = threadIdx.x;
-  unsigned int globalid = blockIdx.x*blockDim.x + threadIdx.x;
+  unsigned int offset = blockIdx.x * blockDim.x;
+  unsigned int globalid = offset + tid;
 
   // For thread ids greater than data space
   if (globalid < N) {
-     sdataVW[tid] =  g_VecV[globalid] * g_VecW[globalid];
-  }
-  else {
-     sdataVW[tid] = 0;  // Case of extra threads above N
+     g_VecV[globalid] =  g_VecV[globalid] * g_VecW[globalid];
   }
 
   // each thread loads one element from global to shared mem
@@ -422,13 +419,13 @@ __global__ void ComputeLamda( float* g_VecV, float* g_VecW, float * g_Lamda,int 
 
   // do reduction in shared mem
   for (unsigned int s=blockDim.x / 2; s > 0; s = s >> 1) {
-     if (tid < s) {
-         sdataVW[tid] = sdataVW[tid] + sdataVW[tid+ s];
+     if ((tid < s) && (tid + s < N)) {
+         g_VecV[offset + tid] = g_VecV[offset + tid] + g_VecV[offset + tid + s]
      }
      __syncthreads();
   }
    // atomic operations:
-  if (tid == 0) atomicAdd(g_Lamda,sdataVW[0]);
+  if (tid == 0) atomicAdd(g_Lamda,g_VecV[offset]);
 }
 
 
